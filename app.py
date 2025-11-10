8591ce075388db402f4263a81f5782065121b9a3
Ilias-Iason Papanikolaou-Polymenidis <=>
2025-11-10 10:49:35 +0200

    Import of the basic structure for the project

---
+++ app.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+from flask import Flask, flash, redirect, render_template, request, session,g
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure application
+app = Flask(__name__)
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+DATABASE = "app.db"
+
+def get_db():
+    if "db" not in g:
+        g.db = sqlite3.connect(DATABASE)
+        g.db.row_factory = sqlite3.Row
+    return g.db
+
+@app.teardown_appcontext
+def close_db(error):
+    db = g.pop("db", None)
+    if db is not None:
+        db.close()
+
+
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)

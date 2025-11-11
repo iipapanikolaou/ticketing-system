e7b5369f5a34080524f6182429348dd8c78d3dbe
Ilias-Iason Papanikolaou-Polymenidis <=>
2025-11-11 17:07:13 +0200

    Basic functionality for homepage and redirect

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import sqlite3
+import re
+import requests
+from helpers import login_required
 
-from flask import Flask, flash, redirect, render_template, request, session,g
+from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -13,21 +16,6 @@ app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
 
-DATABASE = "app.db"
-
-def get_db():
-    if "db" not in g:
-        g.db = sqlite3.connect(DATABASE)
-        g.db.row_factory = sqlite3.Row
-    return g.db
-
-@app.teardown_appcontext
-def close_db(error):
-    db = g.pop("db", None)
-    if db is not None:
-        db.close()
-
-
 
 @app.after_request
 def after_request(response):
@@ -38,8 +26,12 @@ def after_request(response):
     return response
 
 @app.route("/")
+@login_required
 def index():
-    return render_template("index.html")
+
+    firstName = session["first_name"]
+    lastName = session["last_name"]
+    return render_template("index.html",firstName,lastName)
 
 @app.route("/login", methods = ["GET","POST"])
 def login():
@@ -53,18 +45,36 @@ def login():
         username = request.form.get("username")
         password = request.form.get("password")
 
-        app.logger.debug('username:')
-        app.logger.debug(username)
-        app.logger.debug('password')
-        app.logger.debug(password)
         if username is None or password is None:
             flash("Invalid username or password")
             return render_template("login.html")
         
-        flash("ela re")
-        return render_template("login.html")
-    
+        if len(username) == 0 or len(password) == 0:
+            flash("Invalid username or password")
+            return render_template("login.html")
+        
+        url = "https://dummyjson.com/auth/login"
 
+        headers = {"Content-Type": "application/json"}
+
+        payload = {"username":username,"password":password}
+
+        dbResponse = requests.post(
+            method = "POST",
+            url=url,
+            headers=headers,
+            json=payload,
+            verify=False
+        )
+
+        app.logger.debug(dbResponse)
+
+        session["first_name"] = dbResponse["firstName"]
+        session["last_name"] = dbResponse["lastName"]
+        session["profile_pic_url"] = dbResponse["image"]
+        session["user_id"]  =dbResponse["id"]
+
+        return redirect("index.html")
 
 
 if __name__ == "__main__":

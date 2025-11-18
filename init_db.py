54a21e76f29bfdc63a73192ccb5d0a053edf73f1
Ilias-Iason Papanikolaou-Polymenidis <=>
2025-11-18 15:36:07 +0200

    Database creation script

---
+++ init_db.py
@@ -0,0 +1,60 @@
+import sqlite3
+
+def init_db():
+    con = sqlite3.connect("ticketing.db")
+
+    cur = con.cursor()
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            first_name TEXT NOT NULL,
+            last_name TEXT NOT NULL,
+            email TEXT NOT NULL UNIQUE,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+            changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS admins (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER NOT NULL,
+            FOREIGN KEY (user_id) REFERENCES users(id)
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS tickets (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            status TEXT NOT NULL,
+            category TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            changed_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            created_by INTEGER NOT NULL,
+            changed_by INTEGER NOT NULL,
+            FOREIGN KEY (created_by) REFERENCES users(id),
+            FOREIGN KEY (changed_by) REFERENCES users(id)
+        );
+    """)
+
+    cur.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            description TEXT NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            created_by INTEGER NOT NULL,
+            FOREIGN KEY (created_by) REFERENCES users(id)
+        );
+    """)
+
+    con.commit()
+    cur.close()
+    con.close()
+
+
+if __name__ == '__main__':
+    init_db()
